fc8112af0bcadb6e5fb7d0bc95503462c73f8f89
Anton <anton.gap99@gmail.com>
2022-04-28 17:25:19 +0300

    add new item

---
+++ lesson_02/script.js
@@ -0,0 +1,21 @@
+'use strict'
+
+{
+    const item = 'smart проектор philips';
+    const count = 15;
+    const category = 'видео техника';
+    const price = 1200;
+
+    console.log(item);
+    console.log(`общая сумма товара: ${count * price}`);
+}
+
+{
+    const item = 'Телевизор Samsumg';
+    const count = 10
+    const category = 'видео техника';
+    const price = 30000;
+
+    console.log(item);
+    console.log(`общая сумма товара: ${count * price}`);
+}

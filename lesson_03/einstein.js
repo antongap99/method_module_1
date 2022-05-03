c88a533ca641ea323b5110c095f5a12d88373963
Anton.gap <anton.gap99@gmail.com>
2022-05-03 18:01:19 +0300

    finished task

---
+++ lesson_03/einstein.js
@@ -0,0 +1,10 @@
+"use strict";
+const myWeight = '88 кг'
+const speedLight = 3e8;
+
+function calculateEnergy(weight, speed ){
+    return parseFloat(weight) * speed ** 2
+}
+console.log('parseFloat(myWeight): ',typeof parseFloat(myWeight));
+console.log('parseFloat(speedLight): ',typeof parseFloat(speedLight));
+console.log('calculateEnergy: ', calculateEnergy(myWeight, speedLight));

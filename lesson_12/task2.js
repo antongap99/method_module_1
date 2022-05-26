44e1f7615f6ecec82bfbe9219d1cf4eeed5b4b5d
Anton <anton.gap99@gmail.com>
2022-05-26 18:48:28 +0300

    fix: fixed by linter

--- lesson_12/task2.js
+++ lesson_12/task2.js
@@ -1,17 +1,19 @@
 'use strict';
 
 {
-    const randomNumber = (max, min) => {return Math.round(Math.random()*(max - min) + min)} 
-    
-    const generateNumber = (arr) => {
-        arr.push(randomNumber(0, 10));
-        let sumOfElem = arr.reduce((acc, elem) => acc + elem, 0)
+  const randomNumber = (max, min) => {
+    Math.round(Math.random() * (max - min) + min);
+  };
 
-        if(sumOfElem > 50){
-            return arr;
-        }
-        return generateNumber(arr);
+  const generateNumber = (arr) => {
+    arr.push(randomNumber(0, 10));
+    const sumOfElem = arr.reduce((acc, elem) => acc + elem, 0);
+
+    if (sumOfElem > 50) {
+      return arr;
     }
+    return generateNumber(arr);
+  };
 
-   console.log( generateNumber([]));
+  console.log(generateNumber([]));
 }
